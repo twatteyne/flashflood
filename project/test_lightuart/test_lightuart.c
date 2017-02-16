8627132210a1990b1020045ff87aaf31cd1794cd
Thomas Watteyne <twatteyne@gmail.com>
2017-02-16 21:54:22 +0100

    WIP #17. missing: print over UART.

--- project/test_lightuart/test_lightuart.c
+++ project/test_lightuart/test_lightuart.c
@@ -1,14 +1,25 @@
 #include "msp430f1611.h"
 #include "stdint.h"
+#include "adc_sensor.h"
 
 /**
 \brief The program test the energy consumption under LPM4.
 */
 int main(void) {
-   WDTCTL     =  WDTPW + WDTHOLD;                // disable watchdog timer
+    uint16_t lightValue;
+    
+    // disable watchdog timer
+    WDTCTL     =  WDTPW + WDTHOLD;
    
-   DCOCTL     =  DCO0 | DCO1 | DCO2;             // MCLK at 8MHz
-   BCSCTL1    =  RSEL0 | RSEL1 | RSEL2;          // MCLK at 8MHz
-   
-   __bis_SR_register(GIE+LPM4_bits);             // sleep
+    // setup clock speed
+    DCOCTL     =  DCO0 | DCO1 | DCO2;             // MCLK at 8MHz
+    BCSCTL1    =  RSEL0 | RSEL1 | RSEL2;          // MCLK at 8MHz
+    
+    adc_sensor_init();
+    while (1) {
+        lightValue = adc_sens_read_total_solar();
+        lightValue = lightValue; // to avoid that "lightValue" gets optimized away
+    }
+    
+    //__bis_SR_register(GIE+LPM4_bits);              // sleep
 }
